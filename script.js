d47033af4483110e8d15e1b0d3f18cc8be2186fe
Leo310 <leodev310@gmail.com>
2022-08-30 22:09:37 +0200

    Update: fixes, still not really working

--- script.js
+++ script.js
@@ -8,8 +8,9 @@ function V2(x, y) {
   this.x = x;
   this.y = y;
 }
-
+let i = 0
 function Pixel(position, width, height, vel, accel, mass, color) {
+  this.id = i++;
   this.position = position;
   this.width = width;
   this.height = height;
@@ -47,46 +48,39 @@ function createPixels(count, size, vel, accel, mass, color) {
   return pixels;
 }
 
-function rules(pixels1, pixels2, g) {
+function rule(pixels1, pixels2, g) {
   pixels1.forEach((pixel1) => {
     pixels2.forEach((pixel2) => {
       const distanceX = pixel1.position.x - pixel2.position.x;
       const distanceY = pixel1.position.y - pixel2.position.y;
-      const distance = Math.sqrt(distanceX ** 2, distanceY ** 2);
-      let force = (g * pixel1.mass * pixel2.mass) / distance ** 2;
-      force = Math.min(8000, force)
+      let distance = Math.sqrt(distanceX * distanceX + distanceY*distanceY);
+      distance = Math.max(distance, 0.001)
+
+      force = (g * pixel1.mass * pixel2.mass) / distance;
 
       const ratioY = distanceY / distance;
       const ratioX = distanceX / distance;
 
-      pixel1.accel.x = -ratioX * (force / pixel1.mass);
-      pixel1.accel.y = -ratioY * (force / pixel1.mass);
-      pixel2.accel.x = ratioX * (force / pixel2.mass);
-      pixel2.accel.y = ratioY * (force / pixel2.mass);
+      pixel1.accel.x = ratioX * (force / pixel1.mass);
+      pixel1.accel.y = ratioY * (force / pixel1.mass);
 
       pixel1.vel.x += pixel1.accel.x;
       pixel1.vel.y += pixel1.accel.y;
-      pixel2.vel.x += pixel2.accel.x;
-      pixel2.vel.y += pixel2.accel.y;
-
-      pixel1.position.x += pixel1.vel.x / 60;
-      pixel1.position.y += pixel1.vel.y / 60;
-
-      pixel2.position.x += pixel2.vel.x / 60;
-      pixel2.position.y += pixel2.vel.y / 60;
     });
+
+    pixel1.position.x += pixel1.vel.x / 60;
+    pixel1.position.y += pixel1.vel.y / 60;
   });
 }
 
-let pixels = createPixels(1, 50, new V2(30, 30), new V2(0, 0), 1000, "green");
-let pixels2 = createPixels(1, 50, new V2(-30, -30), new V2(0, 0), 1000, "red");
+let pixels = createPixels(2, 50, new V2(0, 0), new V2(0, 0), 100, "green");
+//let pixels2 = createPixels(1, 50, new V2(0, 0), new V2(0, 0), 1000, "red");
 
 function main() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  rules(pixels, pixels2, 20);
+  rule(pixels, pixels, 20);
 
   drawPixels(pixels);
-  drawPixels(pixels2);
   requestAnimationFrame(main);
 }
 main();
