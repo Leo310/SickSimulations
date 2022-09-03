58d1ed3a3f705445081379163a4bab5391452719
Leo310 <leodev310@gmail.com>
2022-09-03 18:43:03 +0200

    Fix: OMG fcking passed same object reference. now working simulation

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext("2d");
 canvas.width = canvas.clientWidth;
 canvas.height = canvas.clientHeight;
 
-function V2(x, y) {
+function Vec(x, y) {
   this.x = x;
   this.y = y;
 }
@@ -25,23 +25,14 @@ function drawPixels(pixels) {
   });
 }
 
-function createPixels(count, size, vel, accel, mass, color) {
+function createPixels(count, size, mass, color) {
   let pixels = [];
   for (let i = 0; i < count; i++) {
-    pixels.push(
-      new Pixel(
-        new V2(
-          Math.floor(Math.random() * (canvas.clientWidth - 400)) + 200,
-          Math.floor(Math.random() * (canvas.clientHeight - 400)) + 200
-        ),
-        size,
-        size,
-        vel,
-        accel,
-        mass,
-        color
-      )
-    );
+    const pixel = new Pixel(
+        new Vec(Math.floor(Math.random() * (canvas.clientWidth - 400)) + 200,
+                Math.floor(Math.random() * (canvas.clientHeight - 400)) + 200),
+        size, size, new Vec(0, 0), new Vec(0, 0), mass, color)
+    pixels.push(pixel);
   }
   return pixels;
 }
@@ -52,48 +43,43 @@ function rule(pixels1, pixels2, g) {
       if (i != j) {
         const distanceX = pixels1[i].position.x - pixels2[j].position.x;
         const distanceY = pixels1[i].position.y - pixels2[j].position.y;
-        // console.log(distanceX, distanceY)
         let distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
-        let force = 0;
-        let ratioY = 0;
-        let ratioX = 0;
-        if (distance > 0 && distance < 800) {
-          force = (g * pixels1[i].mass * pixels2[j].mass) / distance;
+        let force, ratioX, ratioY = 0;
+        Math.max(distance, 1)
+        force = (g * pixels1[i].mass * pixels2[j].mass) / distance;
 
-          ratioY = distanceY / distance;
-          ratioX = distanceX / distance;
-          pixels1[i].accel.x = ratioX * (force / pixels1[i].mass);
-          pixels1[i].accel.y = ratioY * (force / pixels1[i].mass);
-        }
-        // Math.max(force, -1000)
-        // console.log(ratioX)
+        ratioY = distanceY / distance;
+        ratioX = distanceX / distance;
+        pixels1[i].accel.x = ratioX * (force / pixels1[i].mass);
+        pixels1[i].accel.y = ratioY * (force / pixels1[i].mass);
 
-        // console.log(i)
-    // console.log(pixels1[i].vel)
-      // console.log(pixels1[i].accel)
         pixels1[i].vel.x += pixels1[i].accel.x;
         pixels1[i].vel.y += pixels1[i].accel.y;
-        // console.log("DistanceX: %d   DistanceY: %d   Distance: %f   Force: %f   Accel: %f   Vel: %f", distanceX, distanceY, distance, force, pixels1[1].accel.x, pixels[1].vel.x)
+        // console.log(pixels1[0].vel.x)
+        // console.log("DistanceX: %d   DistanceY: %d   Distance: %f   Force: %f
+        // Accel: %f   Vel: %f", distanceX, distanceY, distance, force,
       }
     }
   }
-  // console.log(pixels1[1].vel)
   for (let i = 0; i < pixels1.length; i++) {
-    // console.log(pixels2[i].vel)
+    if(pixels1[i].position.x + pixels1[i].vel.x > canvas.width || pixels1[i].position.x + pixels1[i].vel.x < 0)
+      pixels1[i].vel.x *=-1
+    if(pixels1[i].position.y + pixels1[i].vel.y > canvas.height || pixels1[i].position.y + pixels1[i].vel.y < 0)
+      pixels1[i].vel.y *=-1
     pixels1[i].position.x += pixels1[i].vel.x;
     pixels1[i].position.y += pixels1[i].vel.y;
   }
 }
 
-let pixels = createPixels(2, 50, new V2(0, 0), new V2(0, 0), 100, "yellow");
-//let pixels2 = createPixels(1, 50, new V2(0, 0), new V2(0, 0), 1000, "red");
-
+let pixels = createPixels(10, 20, 10, "yellow");
+// let pixels2 = createPixels(1, 50, new V2(0, 0), new V2(0, 0), 1000, "red");
+console.log(pixels)
 function main() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "steelblue";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  rule(pixels, pixels, -200);
+  rule(pixels, pixels, -5);
 
   drawPixels(pixels);
   requestAnimationFrame(main);
